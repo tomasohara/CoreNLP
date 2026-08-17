e8fe55b62c59dcc4a4f9fecc70bbf8e2c6bf941a
Tom O'Hara <tohara@tohara.com>
2026-08-17 16:57:08 -0400

    uses lambda functions for debug tracing

--- src/edu/stanford/nlp/ie/crf/LinearCliquePotentialFunction.java
+++ src/edu/stanford/nlp/ie/crf/LinearCliquePotentialFunction.java
@@ -1,7 +1,7 @@
 package edu.stanford.nlp.ie.crf;
 import edu.stanford.nlp.util.logging.Redwood;
-// BAD: import edu.stanford.nlp.util.ArrayUtils;
 import java.util.Arrays;
+import java.util.function.Supplier;
 
 /**
  * @author Mengqiu Wang
@@ -15,15 +15,13 @@ public class LinearCliquePotentialFunction implements CliquePotentialFunction {
   private static final Redwood.RedwoodChannels log = Redwood.channels(LinearCliquePotentialFunction.class);
     
   LinearCliquePotentialFunction(float[][] weights) {
-    // BAD: log.debug("in LinearCliquePotentialFunction/1: " + ArrayUtils.toString(weights));
-    log.debug("in LinearCliquePotentialFunction/1: " + Arrays.deepToString(weights));
+    log.debug((Supplier<String>) () -> "in LinearCliquePotentialFunction/1: " + Arrays.deepToString(weights));
     this.fWeights = weights;
     this.dWeights = null;
   }
 
   LinearCliquePotentialFunction(double[][] weights) {
-    // BAD: log.debug("in LinearCliquePotentialFunction/2: " + ArrayUtils.toString(weights));
-    log.debug("in LinearCliquePotentialFunction/2: " + Arrays.deepToString(weights));
+    log.debug((Supplier<String>) () -> "in LinearCliquePotentialFunction/2: " + Arrays.deepToString(weights));
     this.fWeights = null;
     this.dWeights = weights;
   }
@@ -31,7 +29,7 @@ public class LinearCliquePotentialFunction implements CliquePotentialFunction {
   @Override
   public double computeCliquePotential(int cliqueSize, int labelIndex,
                                        int[] cliqueFeatures, double[] featureVal, int posInSent) {
-    log.debug("in computeCliquePotential: " + cliqueSize + "," + labelIndex +
+    log.debug((Supplier<String>) () -> "in computeCliquePotential: " + cliqueSize + "," + labelIndex +
 	      "," + Arrays.toString(cliqueFeatures) + "," + Arrays.toString(featureVal) + "," + posInSent);
     double output = 0.0;
     for (int m = 0; m < cliqueFeatures.length; m++) {
