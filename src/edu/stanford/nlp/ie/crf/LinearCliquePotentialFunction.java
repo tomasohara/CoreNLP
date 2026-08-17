44bb8e274a7e87017c4cc28ff8dbfb4b63fb6784
Tom O'Hara <tohara@tohara.com>
2026-08-17 16:09:15 -0400

    fixes initializer tracing

--- src/edu/stanford/nlp/ie/crf/LinearCliquePotentialFunction.java
+++ src/edu/stanford/nlp/ie/crf/LinearCliquePotentialFunction.java
@@ -1,5 +1,6 @@
 package edu.stanford.nlp.ie.crf;
 import edu.stanford.nlp.util.logging.Redwood;
+// BAD: import edu.stanford.nlp.util.ArrayUtils;
 import java.util.Arrays;
 
 /**
@@ -14,13 +15,15 @@ public class LinearCliquePotentialFunction implements CliquePotentialFunction {
   private static final Redwood.RedwoodChannels log = Redwood.channels(LinearCliquePotentialFunction.class);
     
   LinearCliquePotentialFunction(float[][] weights) {
-   log.debug("in LinearCliquePotentialFunction/1: " + weights);
+    // BAD: log.debug("in LinearCliquePotentialFunction/1: " + ArrayUtils.toString(weights));
+    log.debug("in LinearCliquePotentialFunction/1: " + Arrays.deepToString(weights));
     this.fWeights = weights;
     this.dWeights = null;
   }
 
   LinearCliquePotentialFunction(double[][] weights) {
-    log.debug("in LinearCliquePotentialFunction/2: " + weights);
+    // BAD: log.debug("in LinearCliquePotentialFunction/2: " + ArrayUtils.toString(weights));
+    log.debug("in LinearCliquePotentialFunction/2: " + Arrays.deepToString(weights));
     this.fWeights = null;
     this.dWeights = weights;
   }
@@ -29,7 +32,7 @@ public class LinearCliquePotentialFunction implements CliquePotentialFunction {
   public double computeCliquePotential(int cliqueSize, int labelIndex,
                                        int[] cliqueFeatures, double[] featureVal, int posInSent) {
     log.debug("in computeCliquePotential: " + cliqueSize + "," + labelIndex +
-	      "," + Arrays.toString(cliqueFeatures) + "," + Arrays.toString(featureVal) + posInSent);
+	      "," + Arrays.toString(cliqueFeatures) + "," + Arrays.toString(featureVal) + "," + posInSent);
     double output = 0.0;
     for (int m = 0; m < cliqueFeatures.length; m++) {
       double dotProd = fWeights == null ? dWeights[cliqueFeatures[m]][labelIndex] : fWeights[cliqueFeatures[m]][labelIndex];
