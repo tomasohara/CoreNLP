fd1e3714188df917308116889b759ade1aa69d44
Tom O'Hara <tohara@tohara.com>
2026-08-16 23:41:37 -0400

    adds debug tracing

--- src/edu/stanford/nlp/ie/crf/LinearCliquePotentialFunction.java
+++ src/edu/stanford/nlp/ie/crf/LinearCliquePotentialFunction.java
@@ -1,4 +1,6 @@
 package edu.stanford.nlp.ie.crf;
+import edu.stanford.nlp.util.logging.Redwood;
+import java.util.Arrays;
 
 /**
  * @author Mengqiu Wang
@@ -8,12 +10,17 @@ public class LinearCliquePotentialFunction implements CliquePotentialFunction {
   private final float[][] fWeights;
   private final double[][] dWeights;
 
+  /** A logger for this class */
+  private static final Redwood.RedwoodChannels log = Redwood.channels(LinearCliquePotentialFunction.class);
+    
   LinearCliquePotentialFunction(float[][] weights) {
+   log.debug("in LinearCliquePotentialFunction/1: " + weights);
     this.fWeights = weights;
     this.dWeights = null;
   }
 
   LinearCliquePotentialFunction(double[][] weights) {
+    log.debug("in LinearCliquePotentialFunction/2: " + weights);
     this.fWeights = null;
     this.dWeights = weights;
   }
@@ -21,6 +28,8 @@ public class LinearCliquePotentialFunction implements CliquePotentialFunction {
   @Override
   public double computeCliquePotential(int cliqueSize, int labelIndex,
                                        int[] cliqueFeatures, double[] featureVal, int posInSent) {
+    log.debug("in computeCliquePotential: " + cliqueSize + "," + labelIndex +
+	      "," + Arrays.toString(cliqueFeatures) + "," + Arrays.toString(featureVal) + posInSent);
     double output = 0.0;
     for (int m = 0; m < cliqueFeatures.length; m++) {
       double dotProd = fWeights == null ? dWeights[cliqueFeatures[m]][labelIndex] : fWeights[cliqueFeatures[m]][labelIndex];
@@ -29,6 +38,7 @@ public class LinearCliquePotentialFunction implements CliquePotentialFunction {
       }
       output += dotProd;
     }
+    log.debug("computeCliquePotential() => " + output);
     return output;
   }
 
